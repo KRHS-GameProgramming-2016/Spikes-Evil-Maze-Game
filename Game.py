e1af7edf937784e88e093596fea567aee15ba7ac
TinTanMan <matthew.disalvo@kearsrage.org>
2016-11-29 11:47:38 -0500

    added more

--- Game.py
+++ Game.py
@@ -1,8 +1,23 @@
+import pygame, sys, math, random
 from Level import *
 from Goal import *
 from Player import *
 from Score import *
 from Timer import *
 from Wall import *
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 800 
+height = 600
+size = width, height
+screen = pygame.display.set_mode(size) 
+
+bgColor = r,g,b = 0, 0, 0
 
 
+bgColor = r,g,b
+screen.fill(bgColor)
+pygame.display.flip()
+clock.tick(60)

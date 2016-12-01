4225b32a71ca65c40bda88f5912bae59ac91b1a7
TinTanMan <matthew.disalvo@kearsrage.org>
2016-12-01 11:14:54 -0500

    yup

--- Wall.py
+++ Wall.py
@@ -1,1 +1,7 @@
+import pygame, sys, math, random
 
+class Wall():
+    def __init__(self, image, pos, tileSize=50):
+        self.image = pygame.image.load("Resources/WallImages/"+image)
+        self.image = pygame.transform.scale(self.image, [tileSize,tileSize])
+        self.rect = self.image.get_rect(center = pos) 

2492f8f88ebb7189ba5d8fa593652765b41b2a60
TinTanMan <matthew.disalvo@kearsrage.org>
2016-12-02 11:11:01 -0500

    Fixed it

--- Game.py
+++ Game.py
@@ -14,18 +14,23 @@ height = 600
 size = width, height
 screen = pygame.display.set_mode(size)  
 
-
 bgColor = r,g,b = 0, 0, 0
+
+level = Level("level1.lvl")
+
+walls = level.walls
+
+levelNumber = 1
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        
 
 
 
-
-bgColor = r,g,b
-screen.fill(bgColor)
-pygame.display.flip()
-clock.tick(60)
+    bgColor = r,g,b
+    screen.fill(bgColor)
+    for wall in walls:
+        screen.blit(wall.image, wall.rect)
+    pygame.display.flip()
+    clock.tick(60)

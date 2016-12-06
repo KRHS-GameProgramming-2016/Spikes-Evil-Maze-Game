bd8171d8ca8b8e147076df76eac5000018d46799
Tommy <thomas.mann@kearsarge.org>
2016-12-06 11:51:41 -0500

    did things

--- Player.py
+++ Player.py
@@ -1,6 +1,19 @@
-import sys, pygame
-pygame.init()
+import pygame, sys, math
 
-class Player(BasePlayer):
-    pass
+class Player():
+    def __init__(self, pos=[0,0]):
+        self.image = pygame.image.load("Player.png")
 
+    def move(self):
+        self.didBounceX = False
+        self.didBounceY = False
+        self.speed = [self.speedx, self.speedy]
+        self.rect = self.rect.move(self.speed)
+        
+    def bounceScreen(self):
+        if self.rect.left < 0 or self.rect.right > width:
+            self.speedx = -self.speedx
+            self.didBounceX = True
+        if self.rect.top < 0 or self.rect.bottom > height:
+            self.speedy = -self.speedy
+            self.didBounceY = True

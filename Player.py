a8b7da83bd03842a57bac0d21cf0d0fbf83fdc29
Tommy <thomas.mann@kearsarge.org>
2016-12-09 11:07:56 -0500

    did shit

--- Player.py
+++ Player.py
@@ -1,19 +1,60 @@
-import pygame, sys, math
+  import pygame, sys, math
 
 class Player():
     def __init__(self, pos=[0,0]):
         self.image = pygame.image.load("Player.png")
+        self.rect = self.image.get_rect(center = pos)
+        self.speedx = 0
+        self.speedy = 0
+        self.speed = [self.speedx, self.speedy]
+        self.maxSpeed = 5
 
     def move(self):
-        self.didBounceX = False
-        self.didBounceY = False
         self.speed = [self.speedx, self.speedy]
         self.rect = self.rect.move(self.speed)
         
-    def bounceScreen(self):
+    def go(self, direction):
+        if direction == "up":
+            self.speedy = -self.maxSpeed
+        if direction == "down":
+            self.speedy = self.maxSpeed
+        if direction == "left":
+            self.speedx = -self.maxSpeed
+        if direction == "right":
+            self.speedx = self.maxSpeed 
+            
+        if direction == "stop up":
+            self.speedy = 0
+        if direction == "stop down":
+            self.speedy = 0
+        if direction == "stop left":
+            self.speedx = 0
+        if direction == "stop right":
+            self.speedx = 0
+        
+    def bounceScreen(self, size):
+        width = size[0]
+        height = size[1]
         if self.rect.left < 0 or self.rect.right > width:
             self.speedx = -self.speedx
+            self.move()
+            self.speedx = 0
             self.didBounceX = True
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speedy = -self.speedy
+            self.move()
+            self.speedy = 0
             self.didBounceY = True
+            
+    def bounceWall(self, other):
+        if self.rect.right > other.rect.left and self.rect.left < other.rect.right:
+            if self.rect.bottom > other.rect.top and self.rect.top < other.rect.bottom:
+                self.speedx = -self.speedx
+                self.speedy = -self.speedy
+                self.move()
+                self.speedx = 0
+                self.didBounceX = True
+                self.speedy = 0
+                self.didBounceY = True
+                
+                

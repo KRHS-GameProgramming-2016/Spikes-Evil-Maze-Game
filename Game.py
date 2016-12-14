52bfd736bf3307d8482b60662e476ab26afb9a78
TinTanMan <matthew.disalvo@kearsrage.org>
2016-12-14 11:15:31 -0500

    kl

--- Game.py
+++ Game.py
@@ -1,7 +1,7 @@
 import pygame, sys, math, random
 from Level import *
 from Goal import *
-#from Player import *
+from Player import *
 from Score import *
 from Timer import *
 from Wall import *
@@ -10,7 +10,7 @@ pygame.init()
 clock = pygame.time.Clock()
 
 width = 1200  
-height = 700
+height = 1200
 size = width, height
 screen = pygame.display.set_mode(size)  
 
@@ -20,16 +20,42 @@ level = Level("level1.lvl")
 
 walls = level.walls
 
+
+
+using = "keyboard"
+
 levelNumber = 1
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if using == "keyboard":
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player.go("up")
+                if event.key == pygame.K_DOWN:
+                    player.go("down")
+                if event.key == pygame.K_RIGHT:
+                    player.go("right")
+                if event.key == pygame.K_LEFT:
+                    player.go("left")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    player.go("stop up")
+                if event.key == pygame.K_DOWN:
+                    player.go("stop down")
+                if event.key == pygame.K_RIGHT:
+                    player.go("stop right")
+                if event.key == pygame.K_LEFT:
+                    player.go("stop left")
 
 
 
     bgColor = r,g,b
     screen.fill(bgColor)
+    for player in players:
+        screen.blit(player.image, player.rect)
+    screen.blit(player.image, player.rect)
     for wall in walls:
         screen.blit(wall.image, wall.rect)
     pygame.display.flip()

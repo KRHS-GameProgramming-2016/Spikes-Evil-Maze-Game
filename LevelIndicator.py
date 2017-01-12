26109f9a81c599980bf83a8e8678102589d46a48
TinTanMan <matthew.disalvo@kearsrage.org>
2017-01-12 11:14:56 -0500

    Added

--- LevelIndicator.py
+++ LevelIndicator.py
@@ -7,11 +7,11 @@ class LevelIndicator():
         
         #http://www.1001fonts.com/westmeath-font.html
         self.font = pygame.font.Font("Resources/Fonts/Westmeath/Westmeath Italic.otf", 50)
-        self.image.blit(self.font.render("LevelNumber: " + str(self.value), True, (255,0,0)), [0,0])
+        self.image.blit(self.font.render("Level: " + str(self.value), True, (255,255,255)), [30,0])
         self.rect = self.image.get_rect(topright = pos)
         print pos, self.rect
     
     def set(self, amount = 1):
         self.value = amount
-        self.image = self.font.render("LevelNumber: " + str(self.value), True, (255,0,0))
+        self.image = self.font.render("Level: " + str(self.value), True, (255,0,0))
         #self.rect = self.image.get_rect(topright = self.rect.topright)
